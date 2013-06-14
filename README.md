73e42035bfce72088d2bbee8ee75ea6fe3c7874b
Yariv Katz <yariv@nerdeez.com>
2013-06-14 16:22:19 +0300

    added wormhole file

--- README.md
+++ README.md
@@ -2,3 +2,25 @@ nerdeez-ember
 =============
 
 Common ember scripts that are common across different applications
+
+## wormhole.js
+
+Wormhole - it bridges time and space. 
+Enables ember to do cross domain communication. 
+let's say you have a rest server in this url: www.foo.com
+and let's say that you have you client app running in this url: www.bar.com
+we want to ajax communicate between these two domain but alas we have the same origin problem,
+ajax can't communicate across different domains, OK no worries thank god we have jsonp.
+using jsonp we can make the browser think we are passing javascript and instead pass data.
+Oh but fuck my life what if i want to do a post, delete, put requests like we love to do with a rest server,
+Jsonp only support get requests!!!!!
+no worries there is porthole.js and this script which i made which uses porthole to create ember support for cross domain communications. 
+
+#### Installation
+
+<script src="{{PATH TO vendor}}/jquery.js"></script>
+<script src="{{PATH TO nerdeez-ember}}/porthole.js"></script>
+<script src="{{PATH TO nerdeez-ember}}/wormhole.js"></script>
+
+#### Usage
+
---
+++ wormhole.js
@@ -0,0 +1,204 @@
+/**
+ * creates an ember object for cross domain communication
+ * 
+ * Example:
+ * 
+ * ```javascript
+ * Nerdeez.crossDomain
+ * ```
+ * 
+ * @requires porthole.js
+ */
+
+//create the namespace if the namespace doesnt exist
+if (typeof Nerdeez === "undefined")
+	var Nerdeez = Ember.Namespace.create();
+
+/**
+ * using porthole.js this will be used to create crossdomain ajax communications using iframes
+ * 
+ * @class Wormhole
+ * @namespace Nerdeez
+ * @requires porthole.js
+ * @extends Ember.Object
+ */
+Nerdeez.Wormhole = Ember.Object.extend({
+	
+	/**
+	 * did i finish to create the porthole communication
+	 * 
+	 * @property linked
+	 * @type Boolean
+	 * @default false
+	 * @private
+	 */
+	linked: false,
+	
+	/**
+	 * if didn't finish iframe communication setup then 
+	 * pend the request to use after communication is ready
+	 * 
+	 * @property pending
+	 * @type Array
+	 * @private
+	 */
+    pending: [],
+    
+    /**
+     * pointing head to the current request
+     * 
+     * @property nextRequest
+     * @type Number
+     * @private
+     */
+    nextRequest: 1,
+    
+    /**
+     * hold jquery defered objects
+     * @property
+     * @private
+     * @type Object
+     */
+    deferreds: {},
+    
+    /**
+     * holds the success functions for an ajax requests
+     * @property
+     * @private
+     * @type Object
+     */
+    successFunction: {},
+    
+    /**
+     * holds the fail functions for an ajax requests
+     * @property
+     * @private
+     * @type Object
+     */
+    failFunction: {},
+    
+    /**
+     * holds the server url
+     * set this property to change the default connection url
+     * @property
+     * @public
+     * @type strng
+     */
+    serverUrl: 'http://localhost:8000/',
+	
+	/**
+	 * @private
+	 * 
+	 * - creates an iframe
+	 * - init porthole to communicate with this iframe
+	 * - capture response events
+	 * important: set the serverUrl property to change the default server
+	 *  
+	 * @method init
+	 */
+    init: function() {
+	    	this._super();
+    	
+        // create the iframe
+        var target = "wormhole_iframe";
+        this.iframe = $('<iframe id="' + target +
+            '" name="' + target +
+            '" src="' + WorkerimClient.server_url + 
+            '" style="width: 0; height: 0; border: none; display: none;"></iframe>');
+            $('body').append(this.iframe);
+            
+        // create a porthole.js proxy window to send and receive message from the vault iframe
+        this.windowProxy = new Porthole.WindowProxy(this.get('serverUrl'), target);
+ 
+        // handle messages based on their type
+        var self = this;
+        this.windowProxy.addEventListener(function(message) {
+            var data = $.parseJSON(message.data);
+            switch (data.type) {
+                case "ready": return self.onReady(data);
+                case "response": return self.onResponse(data);
+                default: throw Error("unknown message type: " + data.type);
+            }
+        });
+    },
+
+	/**
+	 * creates an ajax request through the porthole to the server
+	 * @param {Object} params
+	 * @return {Object} the promise for the ajax request
+	 */    
+    ajax: function(params) {
+        var requestId = this.nextRequest;
+        this.nextRequest += 1;
+ 
+        var deferred = $.Deferred();
+        this.deferreds[requestId] = deferred;
+        this.successFunction[requestId] = params.successFunction;
+        this.failFunction[requestId] = params.failFunction;
+        var request = {requestId: requestId, params: params};
+        if (this.linked) {
+            this.sendRequest(request);
+        } else {
+            this.pending.push(request);
+        }
+        return deferred.promise();
+    },
+    
+    /**
+     * send the request through the porthole 
+	 * @param {Object} request
+     */
+    sendRequest: function(request) {
+        try{
+            this.windowProxy.post(JSON.stringify(request));
+        }
+        catch(err){
+            console.log(err);
+        }
+    },
+ 
+    /**
+     * finished opening communication portal with the server
+     * now we can send all the pending requests
+	 * @param {Object} data
+     */
+    onReady: function(data) {
+        this.linked = true;
+        for (var i = 0; i < this.pending.length; i++) {
+            this.sendRequest(this.pending[i]);
+            //this.pending = this.pending.splice(i, 1);
+            //this.sendRequest(this.pending.pop());
+        }
+        this.pending = [];
+    },
+    // handle responses to requests made through the wormhole
+    
+    /**
+     * the server returned an answer run the success or fail function
+	 * @param {Object} data
+     */
+    onResponse: function(data) {
+        var deferred = this.deferreds[data.requestId];
+        delete this.deferreds[data.requestId];
+        if (data.success) {
+            deferred.resolve(data.data, data.textStatus);
+            this.successFunction[data.requestId](data.data);
+        } else {
+            deferred.reject(data.textStatus, data.errorThrown);
+            //alert('Communication error');
+            this.failFunction[data.requestId](data.data, {status: 500, responseText: 'Server error'});
+        }
+    }
+});
+
+//iterate on applications inject and register
+data = Ember.Application.NAMESPACES_BY_ID;
+for (var k in data) {
+    if (data.hasOwnProperty(k)) {
+       app = data[k];
+       app.register('wormhole:current', Nerdeez.Wormhole, {singleton: true});
+       app.inject('controller', 'wormhole', 'wormhole:current');
+       app.inject('view', 'wormhole', 'wormhole:current');
+       app.inject('store', 'wormhole', 'wormhole:current');
+    }
+}

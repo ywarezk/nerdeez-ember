a856dbbff82340eb1965e4422ffeabda58daaa70
Yariv Katz <yariv@nerdeez.com>
2013-06-24 14:27:50 +0300

    added handlebar helper

--- handlebars_helpers.js
+++ handlebars_helpers.js
@@ -1,17 +1,42 @@
-Handlebars.registerHelper('everyNth', function(context, every, options) {
-  var fn = options.fn, inverse = options.inverse;
-  var ret = "";
-  if(context && context.length > 0) {
-    for(var i=0, j=context.length; i<j; i++) {
-      var modZero = i % every === 0;
-      ret = ret + fn(_.extend({}, context[i], {
-        isModZero: modZero,
-        isModZeroNotFirst: modZero && i > 0,
-        isLast: i === context.length - 1
-      }));
-    }
-  } else {
-    ret = inverse(this);
-  }
-  return ret;
+/*var getPath = Ember.Handlebars.getPath;
+Ember.Handlebars.registerHelper('everyNth', function(label, every, options) {
+	var context = (options.fn.contexts && options.fn.contexts[0]) || this;
+	var val1 = getPath(context, label, options.fn);
+	//var val2 = getPath(context, val2, options.fn);
+	var fn = options.fn, inverse = options.inverse;
+	var ret = "";
+	if(val1 && val1.get('length') > 0) {
+	    for(var i=0, j=val1.get('length'); i<j; i++) {
+			var modZero = i % every === 0;
+		    ret = ret + fn(_.extend({}, context[i], {
+		        isModZero: modZero,
+		        isModZeroNotFirst: modZero && i > 0,
+		        isLast: i === val1.get('length') - 1
+		    }));
+	    }
+	} else {
+	    ret = inverse(this);
+	}
+	return ret;
 });
+
+/*Ember.Handlebars.registerBoundHelper('everyNth', function(label, every, options) {
+  var options = Array.prototype.pop.call(arguments);
+  var string = options.data.properties[1];
+  return (count > 1 ? string+"s" : string);
+});*/
+
+WorkerimClient.registerViewHelper('everyNth', Ember.View.extend({
+  tagName: 'span',
+
+  template: Ember.Handlebars.compile('{{view.formattedContent}}'),
+
+  formattedContent: (function() {
+    var content = this.get('content');
+
+    if (content != null) {
+      // Capitalize `content`.
+      return content.charAt(0).toUpperCase() + content.slice(1);
+    }
+  }).property('content')
+}));
